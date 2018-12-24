2e467da44d4d952d92ad7c3299895edb483599f9
Tobias Oberbeck <tobiasoberbeck@gmx.de>
2018-12-24 12:28:09 +0100

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <Scene> <Box> </Box> </Scene>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+coolbox {
+  background-color: blue;
+  width: 100%;
+  height: 100%;
+}
+body {
+  background-color: red;
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,19 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from "vue";
+import App from "./App";
+
+var kp = require("vue-babylonjs");
+Vue.use(kp);
+
+//import vb from "vue-babylonjs";
+//Vue.use(vb);
+
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue({
+  el: "#app",
+  components: { App },
+  template: "<App/>"
+});
